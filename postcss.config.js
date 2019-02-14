0d94e6b03111e6aebd747f95093cbe1b53670236
skcai <304884568@qq.com>
2019-02-14 09:31:49 +0800

    install PostCSS plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-preset-env': {},
+    'cssnano': {
+      preset: 'advanced',
+      autoprefixer: false,
+      'postcss-zindex': false
+    },
+    'postcss-px-to-viewport': {
+      viewportWidth: 750, // (Number) The width of the viewport.
+      viewportHeight: 1334, // (Number) The height of the viewport.
+      unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
+      viewportUnit: 'vw', // (String) Expected units.
+      selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
+      minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
+      mediaQuery: false // (Boolean) Allow px to be converted in media queries.
+    },
+    'postcss-aspect-ratio-mini': {},
+    'postcss-write-svg': { utf8: false }
   }
 }
